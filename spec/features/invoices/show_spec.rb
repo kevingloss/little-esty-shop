0f0915513a9b58187da707ecee9c3df8bdadb242
Kevin Gloss <kgloss89@gmail.com>
2022-01-10 16:08:24 -0700

    Format currency in test

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -47,6 +47,6 @@ RSpec.describe "Merchant invoice show" do
   it 'I see the total revenue that will be generated from all of my items on the invoice' do
     visit merchant_invoice_path(@merchant_1, @invoice_4)
     expect(page).to have_content("Total Revenue")
-    expect(page).to have_content(@invoice_4.total_revenue)
+    expect(page).to have_content(h.number_to_currency(@invoice_4.total_revenue/100, precision: 0))
   end
 end
