8dc0edd759335990950d3aac71c7bcb46a164baa
Robin Dittrich <robinodittrich@gmail.com>
2022-01-11 14:35:10 -0800

    Create Esty class with repo and get_url methods.

---
+++ app/models/poro/esty_service.rb
@@ -0,0 +1,10 @@
+class EstyService
+  def repo
+    get_url("/repos/kevingloss/little-esty-shop")
+  end
+
+  def get_url(url)
+    response = HTTParty.get("https://api.github.com#{url}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
