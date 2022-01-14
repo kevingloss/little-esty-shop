eaac7f50e89431caac877e51f8fc909b3397c67e
Kevin Gloss <kgloss89@gmail.com>
2022-01-14 16:32:03 -0700

    Test validations and relationships

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe Discount, type: :model do
   end
 
   describe 'validations' do
-    it { should validate_presence_of :quantity }
+    it { should validate_presence_of :percent }
     it { should validate_presence_of :threshold }
+    it { should validate_numericality_of(:percent).is_greater_than(0).only_integer }
+    it { should validate_numericality_of(:percent).is_less_than(100).only_integer }
+    it { should validate_numericality_of(:threshold).is_greater_than(0).only_integer }
   end
 end
