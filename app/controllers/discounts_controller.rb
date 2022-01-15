ec222d97e1fa14d24bac5e5aecb49e3dbf441a50
Kevin Gloss <kgloss89@gmail.com>
2022-01-14 17:49:43 -0700

    Add show feature

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -2,4 +2,8 @@ class DiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
+
+  def show
+    @discount = Discount.find(params[:id])
+  end
 end
