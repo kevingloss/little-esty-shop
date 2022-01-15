a9f2c7686a256b92abf144babc085970d159586b
Kevin Gloss <kgloss89@gmail.com>
2022-01-14 18:50:17 -0700

    Complete new/create feats

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -8,6 +8,20 @@ class DiscountsController < ApplicationController
   end
 
   def new
-    require 'pry'; binding.pry
+    # require 'pry'; binding.pry
+    merchant = Merchant.find(params[:merchant_id])
+    @discount = merchant.discounts.new
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    merchant.discounts.create(discount_params)
+
+    redirect_to model: merchant
+  end
+
+  private
+  def discount_params
+    params.require(:discount).permit(:merchant_id, :percent, :threshold)
   end
 end
