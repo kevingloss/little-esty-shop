bcb9686d130704c502a648952ffaf0a6346cff96
Kevin Gloss <kgloss89@gmail.com>
2022-01-17 09:58:12 -0700

    Add relationship to merchants and discounts.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,6 +2,8 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
   has_many :transactions, through: :invoice
+  has_many :merchants, through: :item
+  has_many :discounts, through: :merchants
   validates :quantity, :unit_price, :status, presence: true
   enum status: { pending: 0, packaged: 1, shipped: 2 }
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -11,5 +11,7 @@ RSpec.describe InvoiceItem do
     it { should belong_to(:item) }
     it { should belong_to(:invoice) }
     it { should have_many(:transactions).through(:invoice) }
+    it { should have_many(:merchants).through(:item) }
+    it { should have_many(:discounts).through(:merchants) }
   end
 end
