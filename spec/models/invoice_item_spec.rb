3b1d74f8d8393ff471aca3959d5bc9e18e63e35f
Kevin Gloss <kgloss89@gmail.com>
2022-01-17 15:25:06 -0700

    Test discount_applied and discounted_ii_revenue

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -52,4 +52,25 @@ RSpec.describe InvoiceItem do
     expect(@invoice_4.invoice_items.first.discount_applied).to eq(@d3)
     expect(@invoice_4.invoice_items.last.discount_applied).to eq(nil)
   end
+
+  it '#discounted_ii_revenue' do
+    @merchant_2 = Merchant.create!(name: "Mike")
+
+    @d1 = Discount.create!(merchant_id: @merchant_1.id, percent: 25, threshold: 10)
+    @d2 = Discount.create!(merchant_id: @merchant_1.id, percent: 50, threshold: 20)
+    @d3 = Discount.create!(merchant_id: @merchant_1.id, percent: 75, threshold: 100)
+    @d4 = Discount.create!(merchant_id: @merchant_1.id, percent: 10, threshold: 5)
+    @d5 = Discount.create!(merchant_id: @merchant_2.id, percent: 30, threshold: 3)
+
+    @item_11 = @merchant_2.items.create!(name: "Item_11", description: "Description_11", unit_price: 150)
+
+    @invoice_4.invoice_items.create!(item_id: @item_11.id, quantity: 5, unit_price: @item_11.unit_price, status: 1)
+    @invoice_4.invoice_items.create!(item_id: @item_5.id, quantity: 20, unit_price: 100, status: 0)
+    @invoice_4.invoice_items.create!(item_id: @item_7.id, quantity: 7, unit_price: 200, status: 0)
+    @invoice_4.invoice_items.create!(item_id: @item_10.id, quantity: 10, unit_price: 300, status: 0)
+    @invoice_4.invoice_items.create!(item_id: @item_2.id, quantity: 3, unit_price: 400, status: 0)
+
+    expect(@invoice_4.invoice_items.first.discounted_ii_revenue).to eq(31200)
+    expect(@invoice_4.invoice_items.last.discounted_ii_revenue).to eq(1200)
+  end
 end
