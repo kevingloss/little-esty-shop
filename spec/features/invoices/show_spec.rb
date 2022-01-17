3be3dd71e31c405597a6e59cc173661b3b21cbef
Kevin Gloss <kgloss89@gmail.com>
2022-01-17 16:31:42 -0700

    Test revenue appears on the page and only the merchants items show

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -3,8 +3,14 @@ require 'rails_helper'
 RSpec.describe "Merchant invoice show" do
   before :each do
     seed_db
+    @merchant_2 = Merchant.create!(name: "Mike")
+    @d1 = Discount.create!(merchant_id: @merchant_1.id, percent: 25, threshold: 5)
+    @d2 = Discount.create!(merchant_id: @merchant_1.id, percent: 50, threshold: 8)
+    @d3 = Discount.create!(merchant_id: @merchant_1.id, percent: 75, threshold: 10)
+    @d4 = Discount.create!(merchant_id: @merchant_1.id, percent: 10, threshold: 2)
+    @d5 = Discount.create!(merchant_id: @merchant_2.id, percent: 30, threshold: 3)
   end
-  
+
   it 'shows all the information relation to the invoice' do
     visit merchant_invoice_path(@merchant_1, @invoice_1)
 
@@ -27,8 +33,9 @@ RSpec.describe "Merchant invoice show" do
   end
 
   it 'does not show other merchants items info' do
-    other_merchant = Merchant.create!(name: "Other Merchant")
-    other_merchant_item = other_merchant.items.create!(name: "Other Merchant Item", description: "Description of other merchants item", unit_price: 33)
+    other_merchant = Merchant.create!(name: "Mike")
+    other_merchant_item = other_merchant.items.create!(name: "Item_11", description: "Description_11", unit_price: 150)
+    @invoice_1.invoice_items.create!(item_id: other_merchant_item.id, quantity: 5, unit_price: other_merchant_item.unit_price, status: 1)
 
     visit merchant_invoice_path(@merchant_1, @invoice_1)
 
@@ -51,6 +58,28 @@ RSpec.describe "Merchant invoice show" do
   it 'I see the total revenue that will be generated from all of my items on the invoice' do
     visit merchant_invoice_path(@merchant_1, @invoice_4)
     expect(page).to have_content("Total Revenue")
-    expect(page).to have_content(h.number_to_currency(@invoice_4.total_revenue/100, precision: 0))
+    expect(page).to have_content(h.number_to_currency(@invoice_4.total_merchant_revenue(@merchant_1)/100, precision: 0))
+  end
+
+  xit 'shows the total discounted revenue' do
+    @merchant_2 = Merchant.create!(name: "Mike")
+
+    @d1 = Discount.create!(merchant_id: @merchant_1.id, percent: 25, threshold: 10)
+    @d2 = Discount.create!(merchant_id: @merchant_1.id, percent: 50, threshold: 20)
+    @d3 = Discount.create!(merchant_id: @merchant_1.id, percent: 75, threshold: 100)
+    @d4 = Discount.create!(merchant_id: @merchant_1.id, percent: 10, threshold: 5)
+    @d5 = Discount.create!(merchant_id: @merchant_2.id, percent: 30, threshold: 3)
+
+    @item_11 = @merchant_2.items.create!(name: "Item_11", description: "Description_11", unit_price: 150)
+
+    @invoice_4.invoice_items.create!(item_id: @item_11.id, quantity: 5, unit_price: @item_11.unit_price, status: 1)
+    @invoice_4.invoice_items.create!(item_id: @item_5.id, quantity: 20, unit_price: 100, status: 0)
+    @invoice_4.invoice_items.create!(item_id: @item_7.id, quantity: 7, unit_price: 200, status: 0)
+    @invoice_4.invoice_items.create!(item_id: @item_10.id, quantity: 10, unit_price: 300, status: 0)
+    @invoice_4.invoice_items.create!(item_id: @item_2.id, quantity: 3, unit_price: 400, status: 0)
+
+    visit merchant_invoice_path(@merchant_1, @invoice_4)
+
+    expect(page).to have_content(h.number_to_currency(@invoice_4.merchant_discounted_revenue(@merchant_1)/100, precision: 0))
   end
 end
