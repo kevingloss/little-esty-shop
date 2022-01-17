5a3bdacd1dbf4bdc9ac12ef5c26e5aa849500618
Kevin Gloss <kgloss89@gmail.com>
2022-01-17 16:35:19 -0700

    Add test for total discounted revenue, not merchant specific

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -43,7 +43,27 @@ RSpec.describe Invoice do
     ii_4 = invoice_1.invoice_items.create!(item_id: @item_10.id, quantity: 10, unit_price: 300, status: 0)
     ii_5 = invoice_1.invoice_items.create!(item_id: @item_2.id, quantity: 3, unit_price: 400, status: 0)
 
-    expect(invoice_1.merchant_items(@merchant_1)).to eq([ii_2, ii_3, ii_4, ii_5].sort)
+    expect(invoice_1.merchant_items(@merchant_1)).to eq([ii_2, ii_3, ii_4, ii_5])
+  end
+
+  it '#total_discounted_revenue' do
+    @merchant_2 = Merchant.create!(name: "Mike")
+
+    @d1 = Discount.create!(merchant_id: @merchant_1.id, percent: 25, threshold: 10)
+    @d2 = Discount.create!(merchant_id: @merchant_1.id, percent: 50, threshold: 20)
+    @d3 = Discount.create!(merchant_id: @merchant_1.id, percent: 75, threshold: 100)
+    @d4 = Discount.create!(merchant_id: @merchant_1.id, percent: 10, threshold: 5)
+    @d5 = Discount.create!(merchant_id: @merchant_2.id, percent: 30, threshold: 3)
+
+    @item_11 = @merchant_2.items.create!(name: "Item_11", description: "Description_11", unit_price: 150)
+
+    @invoice_4.invoice_items.create!(item_id: @item_11.id, quantity: 5, unit_price: @item_11.unit_price, status: 1)
+    @invoice_4.invoice_items.create!(item_id: @item_5.id, quantity: 20, unit_price: 100, status: 0)
+    @invoice_4.invoice_items.create!(item_id: @item_7.id, quantity: 7, unit_price: 200, status: 0)
+    @invoice_4.invoice_items.create!(item_id: @item_10.id, quantity: 10, unit_price: 300, status: 0)
+    @invoice_4.invoice_items.create!(item_id: @item_2.id, quantity: 3, unit_price: 400, status: 0)
+
+    expect(@invoice_4.total_discounted_revenue).to eq(37435)
   end
 
   describe 'models' do
