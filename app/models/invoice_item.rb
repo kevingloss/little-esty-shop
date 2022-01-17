d42ac71a3ad3d08555238e81b4bd195cc68a0d5a
Kevin Gloss <kgloss89@gmail.com>
2022-01-17 13:20:41 -0700

    Feat discount applied and discount_ii_revenue.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,8 +2,22 @@ class InvoiceItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :item
   has_many :transactions, through: :invoice
-  has_many :merchants, through: :item
-  has_many :discounts, through: :merchants
+  has_one :merchant, through: :item
+  has_many :discounts, through: :merchant
   validates :quantity, :unit_price, :status, presence: true
   enum status: { pending: 0, packaged: 1, shipped: 2 }
+
+  def discount_applied
+    discounts.order(percent: :desc)
+             .where("threshold <= ?", self.quantity)
+             .first
+  end
+
+  def discounted_ii_revenue
+    if discount_applied == nil
+      quantity * unit_price
+    else
+      quantity * unit_price * (1 - (discount_applied.percent/100.to_f))
+    end
+  end
 end
