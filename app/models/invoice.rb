dc647f61da7ac0d8fcc870677cef87a57e8b94ca
Kevin Gloss <kgloss89@gmail.com>
2022-01-17 19:11:00 -0700

    Comment out pending AR method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -17,7 +17,6 @@ class Invoice < ApplicationRecord
     invoice_items.sum("unit_price * quantity")
   end
 
-# this one works, but feels like too much ruby
   def total_discounted_revenue
     invoice_items.sum do |ii|
       ii.discounted_ii_revenue
@@ -28,17 +27,6 @@ class Invoice < ApplicationRecord
     merchant_items(merchant).sum("invoice_items.unit_price * invoice_items.quantity")
   end
 
-  # def merchant_discounted_revenue(merchant)
-    # require 'pry'; binding.pry
-    # getting closer for this approach
-    # savings = invoice_items.joins(:discounts).where("invoice_items.quantity >= threshold").select("invoice_items.*, max(discounts.percent/100 * invoice_items.quantity * invoice_items.unit_price) as savings").group("invoice_items.id").sum(&:savings)
-  # end
-
-  # def merchant_discounted_revenue(merchant)
-  #   total_merchant_revenue(merchant) - savings
-  # end
-
-# this one works
   def merchant_discounted_revenue(merchant)
     merchant_items(merchant).sum {|invoice_item| invoice_item.discounted_ii_revenue }
   end
@@ -49,4 +37,15 @@ class Invoice < ApplicationRecord
     .order(created_at: :asc)
     .distinct
   end
+
+  # WIP - AR
+    # def merchant_discounted_revenue(merchant)
+    #   require 'pry'; binding.pry
+    #   # getting closer for this approach
+    #   # savings = invoice_items.joins(:discounts).where("invoice_items.quantity >= threshold").select("invoice_items.*, max((discounts.percent/100) * invoice_items.quantity * invoice_items.unit_price) as savings").group("invoice_items.id, discounts.id").sum(&:savings)
+    # end
+
+    # def merchant_discounted_revenue(merchant)
+    #   total_merchant_revenue(merchant) - savings
+    # end
 end
